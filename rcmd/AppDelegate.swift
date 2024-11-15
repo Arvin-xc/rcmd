a1be04eaea0f5836d65944b5d8b2d4805dfae2ae
刘西川 <xichuan.lxc@gmail.com>
2024-11-15 17:43:49 +0800

    init

---
+++ rcmd/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  rcmd
+//
+//  Created by 刘西川 on 11/15/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ rcmd/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  rcmd
+//
+//  Created by 刘西川 on 11/15/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
